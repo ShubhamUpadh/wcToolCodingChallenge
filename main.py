6bd8454db95b286537df293974b49fc0fa0a9171
ShubhamUpad <01shubhamupadhyay@gmail.com>
2024-02-15 19:35:28 +0530

    Completed for number of bytes -c

--- main.py
+++ main.py
@@ -5,18 +5,24 @@ fileMode = sys.argv[1]
 fileName = sys.argv[2]
 
 # Check is fileMode is correct or not
-if len(fileMode) > 2 or fileMode[0] != "-" or fileMode != "-":
+if len(fileMode) > 2 or fileMode[0] != "-":
     print("Improper input format")
-    print("The input format is as")
+    print("The input format is as Hello")
     sys.exit()          # This will exit the program as soon as we get improper input
+
 
 if len(fileMode) > 1 and fileMode[1] not in ("c","l","w","m"):
     print("Invalid command \nUse -c for number of bytes, -l for number of lines, -w for  number of words")
 elif len(fileMode) == 1 and fileMode != "-":
-    print("Invalid command \nUse -c for number of bytes, -l for number of lines, -w for  number of words")
+    print("Invalid command \nUse -c for number of bytes, -l for number of lines, -w for  number of words, - for all of these")
 
 # Check if the fileName is correct or not
 if os.path.isfile("./" + fileName) is False:
     print("File not found")
     sys.exit()          # This will exit the program as soon as we get improper input
+
+# -c Output the number of bytes in file
+if fileMode == "-c":
+    print(str(os.path.getsize("./" + fileName)) + "  " + fileName)
+
 

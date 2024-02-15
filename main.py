38f15900aeb4909ab3cf206adc46baeb573e1544
ShubhamUpad <01shubhamupadhyay@gmail.com>
2024-02-15 19:32:02 +0530

    Improved the check for fileMode

--- main.py
+++ main.py
@@ -10,13 +10,13 @@ if len(fileMode) > 2 or fileMode[0] != "-" or fileMode != "-":
     print("The input format is as")
     sys.exit()          # This will exit the program as soon as we get improper input
 
-if len(fileMode) == 2 and fileMode[1] not in ("c","l","w",,"m"):
-    print("Invalid command \n Use -c for number of bytes, -l for number of lines, -w for  number of words")
-elif fileMode != "-":
-    print("Invalid command \n Use -c for number of bytes, -l for number of lines, -w for  number of words")
+if len(fileMode) > 1 and fileMode[1] not in ("c","l","w","m"):
+    print("Invalid command \nUse -c for number of bytes, -l for number of lines, -w for  number of words")
+elif len(fileMode) == 1 and fileMode != "-":
+    print("Invalid command \nUse -c for number of bytes, -l for number of lines, -w for  number of words")
 
 # Check if the fileName is correct or not
 if os.path.isfile("./" + fileName) is False:
     print("File not found")
     sys.exit()          # This will exit the program as soon as we get improper input
-    
+

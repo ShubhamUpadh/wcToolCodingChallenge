6e71a9b03b0d00a023280783c815f469e6464c5e
ShubhamUpad <01shubhamupadhyay@gmail.com>
2024-02-15 15:20:54 +0530

    Refined the file not existing message

--- sandbox.py
+++ sandbox.py
@@ -8,8 +8,9 @@ if len(currentArgs) != 2 or currentArgs[0][0] != "-" or len(currentArgs[0]) != 2
     sys.exit()
     
 
-
-if os.path.isfile("./" + currentArgs[1]):
+print(currentArgs[1])
+print(os.getcwd())
+if os.path.isfile("./" + currentArgs[1]) is False:
     print("File not found")
 else:
     print("File Found")

8b69da01d1141b15a10ef7d6476d6d236ebf9c09
ShubhamUpad <01shubhamupadhyay@gmail.com>
2024-02-15 19:13:22 +0530

    Cleared up the logic in main file

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os.path
 fileMode = sys.argv[1]
 fileName = sys.argv[2]
 
-if len(fileMode) != 2 or fileMode[0] != "-" or len(fileMode[0]) > 2:
+if len(fileMode) > 2 or fileMode[0] != "-" or fileMode != "-":
     print("Improper input format")
     print("The input format is as")
     sys.exit()          # This will exit the program as soon as we get improper input
@@ -13,6 +13,10 @@ if os.path.isfile("./" + fileName) is False:
     print("File not found")
     sys.exit()          # This will exit the program as soon as we get improper input
 
+if len(fileMode) == 2 and fileMode[1] not in ("c","l","w",,"m"):
+    print("Invalid command \n Use -c for number of bytes, -l for number of lines, -w for  number of words")
+elif fileMode != "-":
+    print("Invalid command \n Use -c for number of bytes, -l for number of lines, -w for  number of words")
 
     
 
--- sandbox.py
+++ sandbox.py
@@ -1,16 +1,21 @@
 import sys
-import os
-print("Command Line Arguments", sys.argv)
-currentArgs = sys.argv[1:]
-if len(currentArgs) != 2 or currentArgs[0][0] != "-" or len(currentArgs[0]) != 2:
-    print("Improper input format")
-    print("The input format is as")
-    sys.exit()
-    
+import os.path
 
-print(currentArgs[1])
-print(os.getcwd())
-if os.path.isfile("./" + currentArgs[1]) is False:
+fileMode = sys.argv[1]
+fileName = sys.argv[2]
+
+if len(fileMode) > 2 or fileMode[0] != "-" or fileMode != "-":
+    print("Improper input format")
+    print("The input format is as Hello")
+    sys.exit()          # This will exit the program as soon as we get improper input
+
+if os.path.isfile("./" + fileName) is False:
     print("File not found")
-else:
-    print("File Found")
+    sys.exit()          # This will exit the program as soon as we get improper input
+
+if len(fileMode) == 2 and fileMode[1] not in ("c","l","w","m"):
+    print("Invalid command \n Use -c for number of bytes, -l for number of lines, -w for  number of words")
+elif fileMode != "-":
+    print("Invalid command \n Use -c for number of bytes, -l for number of lines, -w for  number of words")
+    
+print("ALL SET")

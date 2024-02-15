196805b4bd6245047748057d89784360a871ea8f
ShubhamUpad <01shubhamupadhyay@gmail.com>
2024-02-15 19:15:15 +0530

    Cleared up the logic in main file

--- main.py
+++ main.py
@@ -4,13 +4,10 @@ import os.path
 fileMode = sys.argv[1]
 fileName = sys.argv[2]
 
+# Check is fileMode is correct or not
 if len(fileMode) > 2 or fileMode[0] != "-" or fileMode != "-":
     print("Improper input format")
     print("The input format is as")
-    sys.exit()          # This will exit the program as soon as we get improper input
-
-if os.path.isfile("./" + fileName) is False:
-    print("File not found")
     sys.exit()          # This will exit the program as soon as we get improper input
 
 if len(fileMode) == 2 and fileMode[1] not in ("c","l","w",,"m"):
@@ -18,8 +15,8 @@ if len(fileMode) == 2 and fileMode[1] not in ("c","l","w",,"m"):
 elif fileMode != "-":
     print("Invalid command \n Use -c for number of bytes, -l for number of lines, -w for  number of words")
 
-    
-
-
-
+# Check if the fileName is correct or not
+if os.path.isfile("./" + fileName) is False:
+    print("File not found")
+    sys.exit()          # This will exit the program as soon as we get improper input
     

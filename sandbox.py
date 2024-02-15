75ef9d7c9fa0bc3a827b597d88fa941cac4c6178
ShubhamUpad <01shubhamupadhyay@gmail.com>
2024-02-15 15:16:48 +0530

    Added a message for file not existing

--- sandbox.py
+++ sandbox.py
@@ -1,5 +1,15 @@
 import sys
+import os
 print("Command Line Arguments", sys.argv)
 currentArgs = sys.argv[1:]
-if len(currentArgs) != 2 or currentArgs[0][0] != "-":
+if len(currentArgs) != 2 or currentArgs[0][0] != "-" or len(currentArgs[0]) != 2:
     print("Improper input format")
+    print("The input format is as")
+    sys.exit()
+    
+
+
+if os.path.isfile("./" + currentArgs[1]):
+    print("File not found")
+else:
+    print("File Found")

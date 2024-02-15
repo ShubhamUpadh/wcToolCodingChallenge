490f31216fb272ae0f16201bba872cad49b0dd34
ShubhamUpad <01shubhamupadhyay@gmail.com>
2024-02-15 15:26:21 +0530

    Added all the changes in the main.py file

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import sys
 import os.path
 
-currentArgs = sys.argv[1:]
-if len(currentArgs) != 2 or currentArgs[0][0] != "-" or len(currentArgs[0]) != 2:
+fileMode = sys.argv[1]
+fileName = sys.argv[2]
+
+if len(fileMode) != 2 or fileMode[0] != "-" or len(fileMode[0]) > 2:
     print("Improper input format")
     print("The input format is as")
     sys.exit()          # This will exit the program as soon as we get improper input
 
-if os.path.isfile("./" + currentArgs[1]) is False:
+if os.path.isfile("./" + fileName) is False:
     print("File not found")
     sys.exit()          # This will exit the program as soon as we get improper input
 
